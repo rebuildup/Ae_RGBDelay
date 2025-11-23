c23cda2eac9477ffa1d97e58d5abe8274b2a4bb8
rebuildup <rebuild.up.up@gmail.com>
2025-11-23 12:51:15 +0900

    build: AE_SmartRender.h のインクルードを追加

--- RGBDelay.h
+++ RGBDelay.h
@@ -31,6 +31,7 @@
 #include "AE_EffectCBSuites.h"
 #include "String_Utils.h"
 #include "AE_GeneralPlug.h"
+#include "AE_SmartRender.h"
 #include "AEGP_SuiteHandler.h"
 
 #include "RGBDelay_Strings.h"
