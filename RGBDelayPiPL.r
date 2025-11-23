5e068859b758cd0bd1253ee8778f35d68f7d1a2e
rebuildup <rebuild.up.up@gmail.com>
2025-11-24 01:53:35 +0900

    Fix RGBDelay: change to RELEASE version (524289)
    - Change STAGE_VERSION from PF_Stage_DEVELOP to PF_Stage_RELEASE
    - Update PiPL version from 525313 to 524289
    - Fixes version mismatch error

--- RGBDelayPiPL.r
+++ RGBDelayPiPL.r
@@ -41,7 +41,7 @@ resource 'PiPL' (16000) {
         },
         /* [8] */
         AE_Effect_Version {
-            525313    /* 1.0.0 (DEVELOP, BUILD 1) */
+            524289    /* 1.0.0 (RELEASE, BUILD 1) */
         },
         /* [9] */
         AE_Effect_Info_Flags {
