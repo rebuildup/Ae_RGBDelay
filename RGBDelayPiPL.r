af8fec6a4b250274088f022e652df277eb7c8f51
rebuildup <rebuild.up.up@gmail.com>
2025-11-23 15:51:05 +0900

    feat: macOSアーキテクチャのサポートとプラグインのメタデータを追加

--- RGBDelayPiPL.r
+++ RGBDelayPiPL.r
@@ -24,6 +24,42 @@ resource 'PiPL' (16000) {
         CodeWin64X86 {"EffectMain"},
     #endif
 #else
+    #ifdef AE_OS_MAC
+        CodeMacIntel64 {"EffectMain"},
+        CodeMacARM64 {"EffectMain"},
+    #endif
+#endif
+        /* [6] */
+        AE_PiPL_Version {
+            2,
+            0
+        },
+        /* [7] */
+        AE_Effect_Spec_Version {
+            PF_PLUG_IN_VERSION,
+            PF_PLUG_IN_SUBVERS
+        },
+        /* [8] */
+        AE_Effect_Version {
+            525313
+        },
+        /* [9] */
+        AE_Effect_Info_Flags {
+            0
+        },
+        /* [10] */
+        AE_Effect_Global_OutFlags {
+            33556480
+        },
+        AE_Effect_Global_OutFlags_2 {
+            134222849
+        },
+        /* [11] */
+        AE_Effect_Match_Name {
+            "361do RGBDelay"
+        },
+        /* [12] */
+        AE_Reserved_Info {
             8
         },
         /* [13] */
