4107121aadca298b176cc8d8a8e632bb335d3103
rebuildup <rebuild.up.up@gmail.com>
2025-11-23 21:21:11 +0900

    fix: Render関数にRGB遅延処理を実装し、8ビットおよび16ビットのデータ処理を追加

--- RGBDelay.cpp
+++ RGBDelay.cpp
@@ -1,6 +1,8 @@
 #define NOMINMAX
 #include "RGBDelay.h"
 
+#include <algorithm>
+
 static PF_Err 
 About (	
 	PF_InData		*in_data,
@@ -135,36 +137,48 @@ Render (
 	A_long width = output->width;
 	A_long height = output->height;
 	
-	// 8-bit implementation for now
-	if (!PF_WORLD_IS_DEEP(output)) {
-		A_u_char *in_data_ptr = (A_u_char*)input_worldP->data;
-		A_u_char *out_data_ptr = (A_u_char*)output_worldP->data;
+	// RGB Delay implementation
+	int r_delay = (int)red_delay;
+	int g_delay = (int)green_delay;
+	int b_delay = (int)blue_delay;
+	
+	if (PF_WORLD_IS_DEEP(output)) {
+		// 16-bit
 		A_long rowbytes = output->rowbytes;
-		
-		int r_delay = (int)red_delay;
-		int g_delay = (int)green_delay;
-		int b_delay = (int)blue_delay;
-		
 		for (int y = 0; y < height; y++) {
+			PF_Pixel16 *in_row = (PF_Pixel16*)((char*)input_worldP->data + y * rowbytes);
+			PF_Pixel16 *out_row = (PF_Pixel16*)((char*)output_worldP->data + y * rowbytes);
+			
 			for (int x = 0; x < width; x++) {
-				// Red
 				int rx = std::min((int)width - 1, std::max(0, x - r_delay));
-				// Green
 				int gx = std::min((int)width - 1, std::max(0, x - g_delay));
-				// Blue
 				int bx = std::min((int)width - 1, std::max(0, x - b_delay));
 				
-				A_u_char *p_out = out_data_ptr + y * rowbytes + x * 4;
+				out_row[x].alpha = in_row[x].alpha;
+				out_row[x].red = in_row[rx].red;
+				out_row[x].green = in_row[gx].green;
+				out_row[x].blue = in_row[bx].blue;
+			}
+		}
+	} else {
+		// 8-bit
+		A_long rowbytes = output->rowbytes;
+		for (int y = 0; y < height; y++) {
+			PF_Pixel8 *in_row = (PF_Pixel8*)((char*)input_worldP->data + y * rowbytes);
+			PF_Pixel8 *out_row = (PF_Pixel8*)((char*)output_worldP->data + y * rowbytes);
+			
+			for (int x = 0; x < width; x++) {
+				int rx = std::min((int)width - 1, std::max(0, x - r_delay));
+				int gx = std::min((int)width - 1, std::max(0, x - g_delay));
+				int bx = std::min((int)width - 1, std::max(0, x - b_delay));
 				
-				p_out[1] = (in_data_ptr + y * rowbytes + rx * 4)[1]; // Red
-				p_out[2] = (in_data_ptr + y * rowbytes + gx * 4)[2]; // Green
-				p_out[3] = (in_data_ptr + y * rowbytes + bx * 4)[3]; // Blue
+				out_row[x].alpha = in_row[x].alpha;
+				out_row[x].red = in_row[rx].red;
+				out_row[x].green = in_row[gx].green;
+				out_row[x].blue = in_row[bx].blue;
 			}
 		}
 	}
-
-	return err;
-}
 
 	return err;
 }
@@ -229,13 +243,21 @@ EffectMain(
 									output);
 				break;
 				
+			case PF_Cmd_SEQUENCE_SETUP:
+			case PF_Cmd_SEQUENCE_SETDOWN:
+			case PF_Cmd_SEQUENCE_FLATTEN:
+				break;
+				
+			case PF_Cmd_FRAME_SETUP:
+			case PF_Cmd_FRAME_SETDOWN:
+				break;
+				
 			case PF_Cmd_RENDER:
 				err = Render(	in_data,
 								out_data,
 								params,
 								output);
 				break;
-		}
 	}
 	catch(PF_Err &thrown_err){
 		err = thrown_err;
